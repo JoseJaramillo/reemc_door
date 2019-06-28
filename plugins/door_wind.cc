befb16206d556e49274efe295735b1c64d5477de
JoseJaramillo <josevicentejaramillo@gmail.com>
2019-06-28 14:06:44 +0200

    wind improvement

--- plugins/door_wind.cc
+++ plugins/door_wind.cc
@@ -3,6 +3,7 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
+#include <stdlib.h>
 
 namespace gazebo
 {
@@ -10,9 +11,22 @@ namespace gazebo
   {
     public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
     {
-      // Store the pointer to the model
-      this->model = _parent;
+      // Store the pointer to the model, link and joint
 
+      this->model = _parent;
+      this->link=  this->model->GetChildLink("door_wind::lever");
+      this->joint=  this->model->GetJoint("door_wind::joint_frame_door");
+
+      // Random wind force and direction
+      srand(time(0)); 
+      int wind_yawn_direction = rand() % 360;   //  Random 0-360 degrees
+      float wind_force = float(rand() % 600 + 400)/1000;      //  Random from 400mN to 1N
+      wind.X()= wind_force*cos(wind_yawn_direction*3.1416/180);
+      wind.Y()= wind_force*sin(wind_yawn_direction*3.1416/180);
+      wind.Z()= 0.0;
+      std::cerr  << wind_yawn_direction << " " << wind_force << " " << wind.X() << " " << wind.Y() <<std::endl;
+      
+      
       // Listen to the update event. This event is broadcast every
       // simulation iteration.
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(
@@ -22,18 +36,16 @@ namespace gazebo
     // Called by the world update start event
     public: void OnUpdate()
     {
+      //  Apply force at every interaction
+      link->AddForce(ignition::math::Vector3d(wind.X(), wind.Z(), wind.Y()));
 
-  
-      gazebo::physics::LinkPtr link=  this->model->GetChildLink("door_wind::lever");
-      gazebo::physics::JointPtr joint=  this->model->GetJoint("door_wind::joint_frame_door");
-      if (joint->GetAngle(2).Radian()<-0.7){
-        link->AddRelativeForce(ignition::math::Vector3d(0, 0, 0.8));
-      }
     }
 
     // Pointer to the model
     private: physics::ModelPtr model;
-
+    private: physics::LinkPtr link;
+    private: physics::JointPtr joint;
+    private: ignition::math::Vector3d wind;
     // Pointer to the update event connection
     private: event::ConnectionPtr updateConnection;
   };
